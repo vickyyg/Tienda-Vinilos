d281270be3ddfa2f1a42e4e6aefbec475f818bbd
vickyyg <viicjygaida@gmail.com>
2023-11-29 23:23:32 -0300

    hola

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // Ajusta esta ruta base según la ruta de tu página en GitHub Pages
 export default defineConfig({
-  base: 'https://vickyyg.github.io/Tienda-Vinilos/',
+  base: ' https://vickyyg.github.io/Tienda-Vinilos/',
   plugins: [react()]
 });
